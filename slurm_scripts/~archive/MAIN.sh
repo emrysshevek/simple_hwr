5d9fc96a003e899757d2da5763b1ee4d341dfabf
Taylor <taylornarchibald@gmail.com>
2019-07-23 18:39:53 -0600

    added production scheduling scripts

---
+++ slurm_scripts/~archive/MAIN.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# #SBATCH --export=config=$1  # "warp_online_lr_schedule4"
+#SBATCH --gres=gpu:1
+#SBATCH -C 'rhel7&pascal'
+#SBATCH --mem 16000M
+#SBATCH --ntasks 1
+#SBATCH --output="./warp_online_lr_schedule4_gru.slurm"
+#SBATCH --time 24:00:00
+#SBATCH --mail-user=taylor.archibald@byu.edu   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+#%Module
+
+module purge
+module load cuda/10.1
+module load cudnn/7.6
+
+group_path="/panfs/pan.fsl.byu.edu/scr/grp/fslg_hwr"
+export PATH="${group_path}/env/hwr4_env/bin:$PATH"
+which python
+
+config="warp_online_lr_schedule4_gru"
+cd "${group_path}/taylor_simple_hwr"
+echo $config
+python -u train.py --config $config
+
