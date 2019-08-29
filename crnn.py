c5ca621f142ad32e4ee256d9ce6489e6465d499a
Mason Poggemann <masonfp@byu.edu>
2019-08-29 13:52:28 -0600

    more attetion

--- crnn.py
+++ crnn.py
@@ -4,15 +4,17 @@ from torch import nn
 from utils import *
 import os
 from torch.autograd import Variable
+from torch.nn import functional as F
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+# os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 # Use ResNet?
 # Increase LSTM dropout
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-MAX_LENGTH=60
+MAX_LENGTH = 60
+
 
 class MLP(nn.Module):
     def __init__(self, input_size, classifier_output_dimension, hidden_layers, dropout=.5, embedding_idx=None):
@@ -64,6 +66,7 @@ class MLP(nn.Module):
             embedding = self.classifier[0:self.embedding_idx](input)
             return embedding
 
+
 class BidirectionalRNN(nn.Module):
 
     def __init__(self, nIn, nHidden, nOut, dropout=.5, num_layers=2, rnn_constructor=nn.LSTM):
@@ -82,6 +85,158 @@ class BidirectionalRNN(nn.Module):
         output = output.view(T, b, -1)
 
         return output
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, dimensions, attention_type='general'):
+        super(SelfAttention, self).__init__()
+
+        if attention_type not in ['dot', 'general']:
+            raise ValueError('Invalid attention type selected.')
+
+        self.attention_type = attention_type
+        if self.attention_type == 'general':
+            self.linear_in = nn.Linear(dimensions, dimensions, bias=False)
+
+        self.linear_out = nn.Linear(dimensions * 2, dimensions, bias=False)
+        self.softmax = nn.Softmax(dim=-1)
+        self.tanh = nn.Tanh()
+
+    def forward(self, query, context):
+        batch_size, output_len, dimensions = query.size()
+        query_len = context.size(1)
+
+        if self.attention_type == "general":
+            query = query.view(batch_size * output_len, dimensions)
+            query = self.linear_in(query)
+            query = query.view(batch_size, output_len, dimensions)
+
+        # TODO: Include mask on PADDING_INDEX?
+
+        # (batch_size, output_len, dimensions) * (batch_size, query_len, dimensions) ->
+        # (batch_size, output_len, query_len)
+        attention_scores = torch.bmm(query, context.transpose(1, 2).contiguous())
+
+        # Compute weights across every context sequence
+        attention_scores = attention_scores.view(batch_size * output_len, query_len)
+        attention_weights = self.softmax(attention_scores)
+        attention_weights = attention_weights.view(batch_size, output_len, query_len)
+
+        # (batch_size, output_len, query_len) * (batch_size, query_len, dimensions) ->
+        # (batch_size, output_len, dimensions)
+        mix = torch.bmm(attention_weights, context)
+
+        # concat -> (batch_size * output_len, 2*dimensions)
+        combined = torch.cat((mix, query), dim=2)
+        combined = combined.view(batch_size * output_len, 2 * dimensions)
+
+        # Apply linear_out on every 2nd dimension of concat
+        # output -> (batch_size, output_len, dimensions)
+        output = self.linear_out(combined).view(batch_size, output_len, dimensions)
+        output = self.tanh(output)
+
+        return output, attention_weights
+
+
+class SelfAttentionLayer(nn.Module):
+    def __init__(self, embed_dim):
+        super(SelfAttentionLayer, self).__init__()
+        self.embed_dim = embed_dim
+        self.scale = 1 / np.sqrt(embed_dim)
+        self.linear = nn.Linear(embed_dim, embed_dim * 3)
+        # self.linear = nn.Linear(embed_dim, embed_dim)
+
+    def forward(self, input):
+        n, b, e = input.shape
+        x = input.view(b, n, e)  # view as [batch size, sequence length, embed dim]
+        x_proj = self.linear(x)
+        q, k, v = x_proj.chunk(3, dim=-1)
+        s = F.softmax(torch.bmm(q, k.transpose(1, -1)), dim=-1)
+        z = torch.bmm(s, v).view(n, b, e)
+        print(input)
+        print(z)
+        return z
+        # return torch.bmm(torch.bmm(q, k.transpose(1, -1)), v).view(n, b, e)
+
+
+class AttentionRNN(nn.Module):
+
+    def __init__(self, nIn, nHidden, nOut, dropout=.5, num_layers=8, rnn_constructor=nn.LSTM):
+        super(AttentionRNN, self).__init__()
+        print(f"Creating {rnn_constructor.__name__}: in:{nIn} hidden:{nHidden} dropout:{dropout} layers:{num_layers} out:{nOut}")
+        self.embedding = nn.Linear(nHidden * 2, nOut) # add dropout?
+        self.rnn_layers = nn.ModuleList()
+        for i in range(num_layers):
+            layer = nn.ModuleList()
+            if i == 0:
+                layer.append(rnn_constructor(nIn, nHidden, bidirectional=True, dropout=dropout))
+            else:
+                layer.append(rnn_constructor(nHidden * 2, nHidden, bidirectional=True, dropout=dropout))
+            layer.append(SelfAttention(nHidden * 2))
+            self.rnn_layers.append(layer)
+
+    def forward(self, input):
+        # input [time size, batch size, output dimension], e.g. 404, 8, 1024
+        recurrent = input
+        for rnn_layer, attn_layer in self.rnn_layers:
+            recurrent, _ = rnn_layer(recurrent)
+            l, b, d = recurrent.shape
+            attn_input = recurrent.view(b, l, d)
+            attn_output, attn_weights = attn_layer(attn_input, attn_input)
+            recurrent = attn_output.view(l, b, d)
+
+        T, b, h = recurrent.size()
+        t_rec = recurrent.view(T * b, h)
+
+        output = self.embedding(t_rec)  # [T * b, nOut]
+        output = output.view(T, b, -1)
+
+        return output
+
+
+class Attention(nn.Module):
+    def __init__(self, input_dim, output_dim, n_layers=8):
+        super(Attention, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.n_layers = n_layers
+        self.attn_layers = nn.ModuleList()
+        for i in range(n_layers):
+            self.attn_layers.append(SelfAttention(input_dim))
+        self.fc_layer = nn.Linear(input_dim, output_dim)
+
+    def forward(self, input):
+        l, b, d = input.shape
+        query = input.view(b, l, d)
+        for layer in self.attn_layers:
+            query, weights = layer(query, query)
+        output = self.fc_layer(query.view(l, b, d))
+        return output
+
+
+class AttentionLayer(nn.Module):
+    def __init__(self, input_dim, output_dim, n_head, dim_feedforward=2048, dropout=0.1):
+        super(AttentionLayer, self).__init__()
+        self.self_attn = torch.nn.MultiheadAttention(input_dim, n_head, dropout=dropout)
+        self.linear1 = torch.nn.Linear(input_dim, dim_feedforward)
+        self.dropout = torch.nn.Dropout(dropout)
+        self.linear2 = torch.nn.Linear(dim_feedforward, output_dim)
+
+        self.norm1 = torch.nn.LayerNorm(input_dim)
+        self.norm2 = torch.nn.LayerNorm(output_dim)
+        self.dropout1 = torch.nn.Dropout(dropout)
+        self.dropout2 = torch.nn.Dropout(dropout)
+
+    def forward(self, src, src_mask=None, src_key_padding_mask=None):
+        src2 = self.self_attn(src, src, src, attn_mask=src_mask,
+                              key_padding_mask=src_key_padding_mask)[0]
+        src = src + self.dropout1(src2)
+        src = self.norm1(src)
+        src2 = self.linear2(self.dropout(F.relu(self.linear1(src))))
+        src = src + self.dropout2(src2)
+        src = self.norm2(src)
+        return src
+
 
 class CNN(nn.Module):
     def __init__(self, cnnOutSize=1024, nc=3, leakyRelu=False):
@@ -149,6 +304,7 @@ class CNN(nn.Module):
             conv2 = self.cnn[intermediate_level:](conv)
             return self.post_process(conv2), self.post_process(conv)
 
+
 class CRNN(nn.Module):
     """ Original CRNN
 
@@ -165,6 +321,7 @@ class CRNN(nn.Module):
         conv = self.cnn(input)
         output = self.rnn(conv)
         return output,
+
 
 class CRNN2(nn.Module):
     """ CRNN with writer classifier
@@ -222,6 +379,7 @@ class CRNN2(nn.Module):
 
         return recognizer_output, classifier_output
 
+
 class CRNN_2Stage(nn.Module):
     """ CRNN with writer classifier
         nh: LSTM dimension
@@ -255,6 +413,7 @@ class CRNN_2Stage(nn.Module):
         #print(cnn_rnn_concat.shape)
 
         return recognizer_output, rnn_input
+
 
 class CRNN_UNet(nn.Module):
     """ CRNN with writer classifier
@@ -301,6 +460,42 @@ class CRNN_UNet(nn.Module):
         """
         conv = self.cnn(input)
         rnn_input = conv  # [width/time, batch, feature_maps]
+
+        if online is not None:
+            rnn_input = torch.cat([rnn_input, online.expand(conv.shape[0], -1, -1)], dim=2)
+        recognizer_output = self.rnn(rnn_input)
+        return recognizer_output, rnn_input
+
+
+class attention_CNN(nn.Module):
+    """ CRNN with writer classifier
+    """
+    def __init__(self, cnnOutSize, nc, alphabet_size, rnn_hidden_dim, rnn_layers=2, leakyRelu=False, recognizer_dropout=.5, online_augmentation=False, rnn_constructor=nn.LSTM):
+        super(attention_CNN, self).__init__()
+        self.softmax = nn.LogSoftmax()
+        self.dropout = recognizer_dropout
+        rnn_expansion_dimension = 1 if online_augmentation else 0
+        rnn_in_dim = cnnOutSize + rnn_expansion_dimension
+        self.cnn = CNN(cnnOutSize, nc, leakyRelu=leakyRelu)
+        # self.rnn = BidirectionalRNN(rnn_in_dim, rnn_hidden_dim, alphabet_size, dropout=recognizer_dropout, num_layers=rnn_layers, rnn_constructor=rnn_constructor)
+        self.rnn = Attention(rnn_in_dim, alphabet_size)
+        # self.rnn = AttentionRNN(rnn_in_dim, rnn_hidden_dim, alphabet_size, dropout=recognizer_dropout, num_layers=rnn_layers, rnn_constructor=rnn_constructor)
+        print(self.rnn)
+
+    def forward(self, input, online=None, classifier_output=None):
+        """
+
+        Args:
+            input:
+            online:
+            classifier_output:
+
+        Returns:
+            tuple: normal prediction, refined prediction, normal CNN encoding, nudged CNN encoding
+
+        """
+        conv = self.cnn(input)
+        rnn_input = conv # [width/time, batch, feature_maps]
 
         if online is not None:
             rnn_input = torch.cat([rnn_input, online.expand(conv.shape[0], -1, -1)], dim=2)
@@ -356,6 +551,7 @@ class basic_CRNN(nn.Module):
         recognizer_output = self.rnn(rnn_input)
         return recognizer_output, rnn_input
 
+
 class Nudger(nn.Module):
 
     def __init__(self, rnn_input_dim, nc, rnn_hidden_dim, rnn_layers=2, rnn_dropout=.5, leakyRelu=False):
@@ -391,12 +587,20 @@ class Nudger(nn.Module):
         recognizer_output_refined = recognizer_rnn(nudged_cnn_encoding)
         return recognizer_output_refined, nudged_cnn_encoding
 
+
 def create_CRNN(config):
     check_inputs(config)
     # For apples-to-apples comparison, CNN outsize is OUT_SIZE + EMBEDDING_SIZE
-    crnn = basic_CRNN(cnnOutSize=config['cnn_out_size'], nc=config['num_of_channels'], alphabet_size=config['alphabet_size'], rnn_hidden_dim=config["rnn_dimension"],
-                recognizer_dropout=config["recognizer_dropout"], online_augmentation=config["online_augmentation"], rnn_layers=config["rnn_layers"], rnn_constructor=config["rnn_constructor"])
+    # crnn = basic_CRNN(cnnOutSize=config['cnn_out_size'], nc=config['num_of_channels'], alphabet_size=config['alphabet_size'], rnn_hidden_dim=config["rnn_dimension"],
+    #             recognizer_dropout=config["recognizer_dropout"], online_augmentation=config["online_augmentation"], rnn_layers=config["rnn_layers"], rnn_constructor=config["rnn_constructor"])
+    crnn = attention_CNN(cnnOutSize=config['cnn_out_size'], nc=config['num_of_channels'],
+                      alphabet_size=config['alphabet_size'], rnn_hidden_dim=config["rnn_dimension"],
+                      recognizer_dropout=config["recognizer_dropout"],
+                      online_augmentation=config["online_augmentation"], rnn_layers=config["rnn_layers"],
+                      rnn_constructor=config["rnn_constructor"])
+
     return crnn
+
 
 def create_CRNNClassifier(config, use_writer_classifier=True):
     # Don't use writer classifier
@@ -409,6 +613,7 @@ def create_CRNNClassifier(config, use_writer_classifier=True):
                  mlp_layers=config["mlp_layers"], detach_embedding=config["detach_embedding"],
                  online_augmentation=config["online_augmentation"], use_writer_classifier=use_writer_classifier, rnn_constructor=config["rnn_constructor"])
     return crnn
+
 
 def check_inputs(config):
     if not config["style_encoder"] or config["style_encoder"] in ["2StageNudger", "2Stage"]:
@@ -431,6 +636,7 @@ def check_inputs(config):
         config["rnn_constructor"]=nn.LSTM
     return config
 
+
 def create_2Stage(config):
     check_inputs(config)
     crnn = CRNN_2Stage(rnn_input_dim=config["rnn_input_dimension"], nc=config['num_of_channels'], alphabet_size=config['alphabet_size'], rnn_hidden_dim=config["rnn_dimension"],
@@ -438,11 +644,13 @@ def create_2Stage(config):
                        online_augmentation=config["online_augmentation"], first_rnn_out_dim=128, rnn_constructor=config["rnn_constructor"])
     return crnn
 
+
 def create_Nudger(config):
     check_inputs(config)
     crnn = Nudger(rnn_input_dim=config["rnn_input_dimension"], nc=config['num_of_channels'], rnn_hidden_dim=config["rnn_dimension"],
                             rnn_layers=config["nudger_rnn_layers"], leakyRelu=False, rnn_dropout=config["recognizer_dropout"], rnn_constructor=config["rnn_constructor"])
     return crnn
+
 
 class TrainerBaseline(json.JSONEncoder):
     def __init__(self, model, optimizer, config, ctc_criterion):
