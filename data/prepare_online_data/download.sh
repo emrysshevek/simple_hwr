4dccba311a4e06313c7f829a15739059383244eb
Aaron Chan <aaron.y.chan64@gmail.com>
2019-09-26 12:19:54 -0600

    added standardized user to data generation scripts

--- data/prepare_online_data/download.sh
+++ data/prepare_online_data/download.sh
@@ -1,5 +1,4 @@
-read -p "IAM On-Line Handwriting DB username: " iam_username
-wget http://www.fki.inf.unibe.ch/DBs/iamOnDB/data/original-xml-all.tar.gz http://www.fki.inf.unibe.ch/DBs/iamOnDB/data/lineImages-all.tar.gz http://www.fki.inf.unibe.ch/DBs/iamOnDB/data/writers.xml --user $iam_username --ask-password
+wget http://www.fki.inf.unibe.ch/DBs/iamOnDB/data/original-xml-all.tar.gz http://www.fki.inf.unibe.ch/DBs/iamOnDB/data/lineImages-all.tar.gz http://www.fki.inf.unibe.ch/DBs/iamOnDB/data/writers.xml --user nnmllab --password datasets
 
 echo "Decompressing original xml"
 mkdir original-xml-all
