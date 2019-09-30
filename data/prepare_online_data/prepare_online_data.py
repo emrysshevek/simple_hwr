52574033ec5bbc9f0a959d9bef545d666c8e39ef
Taylor <taylornarchibald@gmail.com>
2019-09-29 22:41:09 -0600

    fixed online GTs

--- data/prepare_online_data/prepare_online_data.py
+++ data/prepare_online_data/prepare_online_data.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from os import getcwd, walk
 from os.path import join, normpath, basename, exists
 from html import unescape
+from pathlib import Path
 
 def clean_text(s):
     return unescape(s)
@@ -17,7 +18,7 @@ def strip_rightmost_alphabetic(s):
             break
     return "".join(L)
 
-def get_image_path_from_id(img_id):
+def get_image_path_from_id(img_id, IMG_DIR):
     components = img_id.split("-")
     folder_1 = components[0]
     folder_2 = "-".join([components[0], strip_rightmost_alphabetic(components[1])])
@@ -34,8 +35,17 @@ def get_xml_files(xml_dir):
         xml_files += [join(root, file) for file in files]
     return xml_files
 
+def check_uniqueness(img_json):
+    # Verify uniqueness
+    from collections import defaultdict
+    counts = defaultdict(int)
+    for item in img_json:
+        counts[item["image_path"]] += 1
+    for key, item in counts.items():
+        if item > 1:
+            print(key, item)
 
-if __name__ == "__main__":
+def main():
     IMG_DIR = 'lineImages'
     XML_DIR = 'original-xml-all'
     img_json = []
@@ -53,13 +63,27 @@ if __name__ == "__main__":
         for line in transcription.findall('TextLine'):
             gt = clean_text(line.get('text'))
             img_id = line.get('id')
-            img_path = get_image_path_from_id(img_id)
+
+            # Correction needed
+            img_id = img_id.split("-")
+            f = Path(file)
+            z_or_w = f.stem[-1] if f.stem[-1] in ["z","w"] else ""
+            img_id2 = str(f.parent.name) + z_or_w
+            img_id = f"{img_id2}-{img_id[2]}"
+
+            img_path = get_image_path_from_id(img_id, IMG_DIR)
             if exists(img_path):
                 full_img_path = prepend_cwd(img_path)
                 img_json.append({'gt': gt, 'image_path': full_img_path, 'augmentation': True})
-    
+
+    print(f"Found {len(img_json)} images")
     if img_json:
         with open('online_augmentation.json', 'w') as fp:
             json.dump(img_json, fp, indent=2)
     else:
         raise Exception("No images found!")
+    return img_json
+
+if __name__ == "__main__":
+    img_json = main()
+    check_uniqueness(img_json)
