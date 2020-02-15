fa3ef2ee6cc3aaf0f39a56f08027dfbf7a3e1270
Taylor Archibald <tahlor@gmail.com>
2020-02-15 10:46:41 -0700

    update

--- models/stroke_model.py
+++ models/stroke_model.py
@@ -24,6 +24,7 @@ class StrokeRecoveryModel(nn.Module):
             return self._forward(input)
         else:
             with torch.no_grad():
+                print("EVAL MODE")
                 return self._forward(input)
 
     def _forward(self, input):
@@ -36,3 +37,35 @@ class StrokeRecoveryModel(nn.Module):
 def fake_cnn(img):
     b, c, h, w = img.shape
     return torch.ones(w + 3 + (w+1) % 2, b, 64)
+
+
+class StartEndPointReconstructor(nn.Module):
+    def __init__(self, vocab_size=5, device="cuda", cnn_type="default64", first_conv_op=CoordConv, first_conv_opts=None, **kwargs):
+        super().__init__()
+        self.__dict__.update(kwargs)
+        if first_conv_op:
+            first_conv_op = CoordConv
+
+        self.start_point_embedding = nn.linear(6, 16)
+        if not is_dalai():
+            self.rnn = BidirectionalRNN(nIn=1024+16, nHidden=128, nOut=vocab_size, dropout=.5, num_layers=2, rnn_constructor=nn.LSTM)
+            self.cnn = CNN(nc=1, first_conv_op=first_conv_op, cnn_type=cnn_type, first_conv_opts=first_conv_opts)
+        else:
+            self.rnn = BidirectionalRNN(nIn=64, nHidden=1, nOut=vocab_size, dropout=.5, num_layers=1,
+                                        rnn_constructor=nn.LSTM)
+            self.cnn = fake_cnn
+            print("DALAi!!!!")
+
+    def forward(self, **kwargs):
+        if self.training:
+            return self._forward(**kwargs)
+        else:
+            with torch.no_grad():
+                return self._forward(**kwargs)
+
+    def _forward(self, start_end_points, image):
+        embedding = self.start_point_embedding(start_end_points)
+        cnn_output = self.cnn(input)
+        rnn_output = self.rnn(torch.cat([cnn_output, embedding], dim=-1)) # width, batch, alphabet
+        # sigmoids are done in the loss
+        return rnn_output
