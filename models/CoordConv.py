4fd4fbeba6f81b43ac67f4ad412dd9803405787e
Taylor Archibald <tahlor@gmail.com>
2020-02-17 02:55:10 -0700

    update

--- models/CoordConv.py
+++ models/CoordConv.py
@@ -11,7 +11,7 @@ An alternative implementation for PyTorch with auto-infering the x-y dimensions.
 
 class AddCoords(nn.Module):
 
-    def __init__(self, with_r=False, zero_center=True, rectangle_x=False, both_x=False):
+    def __init__(self, with_r=False, zero_center=True, rectangle_x=False, both_x=False, with_sin=True):
         """ Include a rectangle and non-rectangle x
 
         Args:
@@ -25,68 +25,79 @@ class AddCoords(nn.Module):
         self.rectangle_x = rectangle_x
         self.zero_center = zero_center
         self.both = both_x
+        self.with_sin = with_sin
 
     def forward(self, input_tensor):
         """
         Args:
             input_tensor: shape(batch, channel, x_dim, y_dim)
         """
-        batch_size, _, x_dim, y_dim = input_tensor.size()
+        batch_size, _, height, width = input_tensor.size()
 
-        xx_channel = torch.arange(x_dim).repeat(1, y_dim, 1)
-        yy_channel = torch.arange(y_dim).repeat(1, x_dim, 1).transpose(1, 2)
+        height_channel = torch.arange(height).repeat(1, width, 1)
+        width_channel = torch.arange(width).repeat(1, height, 1).transpose(1, 2)
 
         # Rescale from 0 to 1
-        xx_channel = xx_channel.float() / (x_dim - 1)
-        yy_channel = yy_channel.float() / (y_dim - 1)
+        height_channel = height_channel.float() / (height - 1)
+        width_channel = width_channel.float() / (width - 1)
 
         # Rescale from -1 to 1
         if self.zero_center:
-            yy_channel = yy_channel * 2 - 1
-            xx_channel = xx_channel * 2 - 1
+            width_channel = width_channel * 2 - 1
+            height_channel = height_channel * 2 - 1
 
         if self.both: # Create default CoordConv, and one scaled to be same scale as y CoordConv
-            xx_rec_channel = xx_channel * x_dim / y_dim
+            xx_rec_channel = width_channel * width / height
 
-            xx_channel = xx_channel.repeat(batch_size, 1, 1, 1).transpose(2, 3)
-            yy_channel = yy_channel.repeat(batch_size, 1, 1, 1).transpose(2, 3)
+            height_channel = height_channel.repeat(batch_size, 1, 1, 1).transpose(2, 3)
+            width_channel = width_channel.repeat(batch_size, 1, 1, 1).transpose(2, 3)
             xx_rec_channel = xx_rec_channel.repeat(batch_size, 1, 1, 1).transpose(2, 3)
 
             ret = torch.cat([
                 input_tensor,
-                xx_channel.type_as(input_tensor),
+                height_channel.type_as(input_tensor),
                 xx_rec_channel.type_as(input_tensor),
-                yy_channel.type_as(input_tensor)], dim=1)
+                width_channel.type_as(input_tensor)], dim=1)
+
+            if self.with_sin:
+                xx_sine_channel = torch.sin(xx_rec_channel * 4)
+                ret = torch.cat([ret, xx_sine_channel.type_as(input_tensor)], dim=1)
+
         else:
             if self.rectangle_x:
-                xx_channel *= x_dim / y_dim
+                width_channel *= width / height
 
-            xx_channel = xx_channel.repeat(batch_size, 1, 1, 1).transpose(2, 3)
-            yy_channel = yy_channel.repeat(batch_size, 1, 1, 1).transpose(2, 3)
+            height_channel = height_channel.repeat(batch_size, 1, 1, 1).transpose(2, 3)
+            width_channel = width_channel.repeat(batch_size, 1, 1, 1).transpose(2, 3)
 
             ret = torch.cat([
                 input_tensor,
-                xx_channel.type_as(input_tensor),
-                yy_channel.type_as(input_tensor)], dim=1)
+                height_channel.type_as(input_tensor),
+                width_channel.type_as(input_tensor)], dim=1)
 
         if self.with_r:
-            rr = torch.sqrt(torch.pow(xx_channel.type_as(input_tensor) - 0.5, 2) + torch.pow(yy_channel.type_as(input_tensor) - 0.5, 2))
+            rr = torch.sqrt(torch.pow(height_channel.type_as(input_tensor) - 0.5, 2) + torch.pow(width_channel.type_as(input_tensor) - 0.5, 2))
             ret = torch.cat([ret, rr], dim=1)
 
         return ret
 
 class CoordConv(nn.Module):
-    def __init__(self, in_channels, out_channels, with_r=False, verbose=False, zero_center=True, rectangle_x=False, both_x=False, **kwargs):
+    def __init__(self, in_channels, out_channels, with_r=False, verbose=False, zero_center=True, rectangle_x=False, both_x=False, with_sin=True, **kwargs):
         super().__init__()
-        self.addcoords = AddCoords(with_r=with_r, zero_center=zero_center, rectangle_x=rectangle_x, both_x=both_x)
+        self.addcoords = AddCoords(with_r=with_r, zero_center=zero_center, rectangle_x=rectangle_x, both_x=both_x, with_sin=with_sin)
         self.verbose = verbose
         in_size = in_channels+2
         if with_r or both_x:
             in_size += 1
+        if with_sin:
+            in_size += 1
         self.conv = nn.Conv2d(in_size, out_channels, **kwargs)
-        logger.info(f"Larger X Coord: {rectangle_x or both_x}")
-        logger.info(f"Normal X Coord: {not rectangle_x or both_x}")
-        logger.info(f"Using 2 X Coord Channels: {both_x}")
+        logger.info(f"Zero center {zero_center}")
+        logger.info(f"RECT X Coord: {rectangle_x or both_x}")
+        logger.info(f"Normalized X Coord: {not rectangle_x or both_x}")
+        logger.info(f"Using ABS+REL X Coord Channels: {both_x}")
+        logger.info(f"X: {with_sin}")
+        logger.info(f"Y: {with_sin}")
 
     def forward(self, x):
         ret = self.addcoords(x)
